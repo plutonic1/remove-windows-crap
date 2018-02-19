96f651f752a3ac12a9dc401028d67a64f7c56128
plutonic1 <plutonic@freenet.de>
2018-02-19 15:54:04 +0100

    y for english and j for german

--- remove_cortana.bat
+++ remove_cortana.bat
@@ -1,5 +1,6 @@
 taskkill /IM searchui.exe /F
 taskkill /IM RuntimeBroker.exe /F
+takeown /f "C:\windows\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy" /r /d j
 takeown /f "C:\windows\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy" /r /d y
 icacls "C:\Windows\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy" /INHERITANCE:e /GRANT:r %UserName%:(F) /T /C
 rmdir /S /Q C:\windows\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy
